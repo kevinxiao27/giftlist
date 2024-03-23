bf1cb68d752df2b3d8e0a51eaaadfc213bad80e4
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-03-23 13:12:54 -0700

    Feat: set up mongoDB

---
+++ giftappbackend/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express from "express";
+import giftRouter from "./views/gift-router.js";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use("/gifts", giftRouter);
+
+mongoose.connect(`${process.env.MONGODB_URI}`).catch((e) => console.log(e));
+
+app.listen(8080, async () => {
+  console.log("Connected to MongoDB and running at http://localhost:8080");
+});
