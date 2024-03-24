2321d806f72478e7144c371f4343438bc25b24bd
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-03-23 18:54:49 -0700

    Feat: add cors

--- giftappbackend/index.js
+++ giftappbackend/index.js
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import giftRouter from "./views/gift-router.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/gifts", giftRouter);
+app.use(cors());
 
 mongoose.connect(`${process.env.MONGODB_URI}`).catch((e) => console.log(e));
 
